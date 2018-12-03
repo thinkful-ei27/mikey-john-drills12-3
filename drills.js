7bbeae684b2d02840f83dd5347c96e6f7629973a
John Young <john.young@spreadstreet.io>
2018-12-03 14:22:36 -0500

    Added in number and string TypeErrors

--- drills.js
+++ drills.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function yearOfBirth(age){
 if (age < 1){ throw new Error('Age cannot be negative')};
 return new Date().getFullYear() - age;
@@ -5,11 +7,20 @@ return new Date().getFullYear() - age;
 }
 
 function createGreeting(name, age){
-  
+    if (name === undefined || age === undefined) {
+        throw new Error('Arguments not valid');
+    }
+    if (typeof age !== 'number') {
+        throw new TypeError('Not a number, please enter a number');
+    }
+
+    if (typeof name !== 'string') {
+        throw new TypeError('Not a string. Enter a correct string');
+    }
     return `Hi my name is ${name} and I\'m ${age} years old. I was born in ${yearOfBirth(age)}`;
 }
 try{
-    console.log(createGreeting('john', -50));
+    console.log(createGreeting(42, 42));
  }
 catch(e){ 
     console.log(e.message);
