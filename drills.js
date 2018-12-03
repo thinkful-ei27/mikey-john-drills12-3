eca6070c90cef105c9c51224073ecbf302d1203f
mikeymanoguerra <mikeymanoguerra@gmail.com>
2018-12-03 14:04:03 -0500

    added year variable, called date

--- drills.js
+++ drills.js
@@ -1,6 +1,9 @@
+
+
 function createGreeting(name, age){
-   return `Hi my name is ${name} and I\'m ${age} years old`;
+    const yearOfBirth= new Date().getFullYear() - age;
+   return `Hi my name is ${name} and I\'m ${age} years old. I was born in ${yearOfBirth}`;
 }
 
-console.log(createGreeting('john', 300));
+console.log(createGreeting('john', 30));
 
