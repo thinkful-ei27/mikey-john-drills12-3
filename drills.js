e36d75953f097b6265979a1d21b05f9ca46fd50b
mikeymanoguerra <mikeymanoguerra@gmail.com>
2018-12-03 14:13:10 -0500

    error for negative age

--- drills.js
+++ drills.js
@@ -1,11 +1,16 @@
 function yearOfBirth(age){
-   return new Date().getFullYear() - age;
+if (age < 1){ throw new Error('Age cannot be negative')};
+return new Date().getFullYear() - age;
+
 }
 
 function createGreeting(name, age){
   
-   return `Hi my name is ${name} and I\'m ${age} years old. I was born in ${yearOfBirth(age)}`;
+    return `Hi my name is ${name} and I\'m ${age} years old. I was born in ${yearOfBirth(age)}`;
 }
-
-console.log(createGreeting('john', 30));
-
+try{
+    console.log(createGreeting('john', -50));
+ }
+catch(e){ 
+    console.log(e.message);
+}
