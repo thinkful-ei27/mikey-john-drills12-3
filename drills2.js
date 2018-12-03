47630a5323b9ad13b6c86f74e3b625e9c1f4766e
John Young <john.young@spreadstreet.io>
2018-12-03 14:35:24 -0500

    Added beyond function

--- drills2.js
+++ drills2.js
@@ -9,3 +9,17 @@ function jediName(firstName, lastName) {
 }
 
 console.log(jediName('John', 'Young'));
+
+function beyond(num) {
+    // Logs different msg depending upon value
+    // if infinite shout 'And beyond'
+    // if finite and positive log 'To infinity'
+    // if finite and negative log 'To negative infinity'
+    // if zero log 'Staying home'
+    if (num === Infinity) return 'And beyond';
+    if (num > 0) return 'To infinity';
+    if (num < 0) return 'To negative infinity';
+    if (num === 0) return 'Staying home';
+}
+
+console.log(beyond(0));
